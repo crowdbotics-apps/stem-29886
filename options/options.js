16ccf93dc33c51602724131e04b4e39366ae27a3
Crowdbotics <team@crowdbotics.com>
2021-08-17 17:47:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "stem_29886Identifier",
+  url: "https://stem_29886Identifier.botics.co",
+  api: "https://stem_29886Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
